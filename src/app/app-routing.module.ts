f2e250c71b541ccf4fc789f9f00e1707429b7190
Cr3xy <pchiduck@gmail.com>
2023-01-18 14:05:18 +0100

    Login working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {EmployeeDetailsComponent} from "./employee-details/employee-details.component";
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/employees', pathMatch: 'full'},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
   {path: 'employees', component: EmployeeListComponent},
   {path: 'employee/:id', component: EmployeeDetailsComponent},
   {path: 'employee/edit/:id', component: EmployeeDetailsComponent},
