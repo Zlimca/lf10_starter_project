899b05c4873d20e3905243aa3986ea1dc27e67bc
Joshua <joshua107@schule.bremen.de>
2023-02-08 15:08:08 +0100

    commit

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -26,6 +26,23 @@ export class EmployeeListComponent implements OnInit {
   }
 
   getEmployees(): void {
-    this.employeeService.getEmployees().subscribe(employees => this.employees = employees);
+    this.employeeService.getEmployees().subscribe(employees => this.setEmployees(employees));
+  }
+
+  setEmployees(employees: Employee[]): void {
+    this.employees = employees
+
+    if (this.employees.length < 2) return
+
+    this.employees = this.employees.sort(function(a, b){ // @ts-ignore
+      return a.id-b.id});
+  }
+
+  getImgId(employeeId: number | undefined): number {
+    return this.employees.findIndex(e => e.id === employeeId) + 1
+  }
+
+  updateImageId(employeeId: number | undefined){
+    localStorage.setItem('imageId', JSON.stringify(this.employees.findIndex(e => e.id === employeeId) + 1))
   }
 }
